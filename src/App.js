5fee2acf155486845b9890c08b35dca76cecbfcf
Sivarajan <citysiva180@gmail.com>
2020-08-20 07:48:45 +0530

    initial_Commit-NavBars&Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import portfolio from "./components/portfolio";
+import contact from "./components/contactMe";
+import article from "./components/article";
+import Navbar from "./utilities/Nabar";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Switch>
+          <div className="container-fluid">
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/portfolio" exact component={portfolio}></Route>
+            <Route path="/contact" exact component={contact}></Route>
+            <Route path="/article" exact component={article}></Route>
+            <Redirect path="/" exact to="/"></Redirect>
+            <Redirect path="/home" exact component={Home}></Redirect>
+          </div>
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/article.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Article extends Component {
+  state = {};
+  render() {
+    return <h1>Article Page</h1>;
+  }
+}
+
+export default Article;
