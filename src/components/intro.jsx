6ae55f6ff26fc0f70f1d2ca10979f4d6e9433f4b
Sivarajan <citysiva180@gmail.com>
2020-08-20 21:59:16 +0530

    paracommit

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -4,17 +4,18 @@ const Introduction = () => {
   return (
     <div className="container">
       <h1>Fullstack Developer / Designer</h1>
+      <p>I humbly welcome you to my site! Here goes my story in Tech...</p>
       <p>
         {" "}
-        I started late in computing. Major reason being that in my village
-        electricity was of rarity. The first time I owned a computer was in my
+        I started late in computing. Major reason being that even electricity in
+        my village was of rarity. The first time I owned a computer was in my
         14s. I got a windows xp computer and spend n number of hours looking
         into it. I did not get to start early. I did my first program in my
         college final year. This too, was due campus recruitment. But there, I
         got my inspiration. My first sucessful program which was to factorize a
         quadratic equation. What took me ages to calculate and compile took
         seconds to program its logic. It not only simplified my problems over
-        the hour, It gave me a confidence that I too could program. There
+        the hour, It gave me a confidence that I too could program. There I
         started my journey. After hours and hours of trials and errors I learnt
         all creamy layers of tech, HTML, CSS and JavaScript and then non creamy
         layers, Linux commands, deployments and networks etc. The more I was in
---
+++ src/components/programming.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Programming = () => {
+  return <div className="container"></div>;
+};
+
+export default Programming;
