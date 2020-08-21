fa6f8643a15900c9108032fd5f7fb900ed8f0272
Sivarajan <citysiva180@gmail.com>
2020-08-21 11:32:32 +0530

    card_formatting

--- src/components/certifications.jsx
+++ src/components/certifications.jsx
@@ -4,19 +4,22 @@ import "../stylesheets/certificate.css";
 
 const Certificates = () => {
   return (
-    <div className="container">
-      <h1>Certificates</h1>
-      <div className="certificate">
+    <div className="certificate">
+      <div className="container">
+        <h1>Certificates</h1>
         <div class="card bg-white text-dark">
           {/* <img src={nyc} class="card-img" alt="..." /> */}
           <div class="card-img-overlay">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Basics in JavaScript</h5>
+            <i class="fas fa-award"></i>
             <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+              <b></b>
+              Certification done through www.codewithmosh.com. Completed object
+              oriented and regular Javascript.
             </p>
-            <p class="card-text">Last updated 3 mins ago</p>
+            <p class="card-text">
+              <b>Received : March 2018</b>
+            </p>
           </div>
         </div>
       </div>
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,27 +2,30 @@ import React from "react";
 import "../stylesheets/Intro.css";
 const Introduction = () => {
   return (
-    <div className="container">
-      <h1>Fullstack Developer / Designer</h1>
-      <p>I humbly welcome you to my site! Here goes my story in Tech...</p>
-      <p>
-        {" "}
-        I started late in computing. Major reason being that even electricity in
-        my village was of rarity. The first time I owned a computer was in my
-        14s. I got a windows xp computer and spend n number of hours looking
-        into it. I did not get to start early. I did my first program in my
-        college final year. This too, was due campus recruitment. But there, I
-        got my inspiration. My first sucessful program which was to factorize a
-        quadratic equation. What took me ages to calculate and compile took
-        seconds to program its logic. It not only simplified my problems over
-        the hour, It gave me a confidence that I too could program. There I
-        started my journey. After hours and hours of trials and errors I learnt
-        all creamy layers of tech, HTML, CSS and JavaScript and then non creamy
-        layers, Linux commands, deployments and networks etc. The more I was in
-        this, the more I wanted to move forward. At times I appreciate that I
-        get to start late cause by the time I started, I had clear Idea what I
-        should do and not do. This is my story in tech.
-      </p>
+    <div className="Introduction">
+      <div className="container">
+        <h1>Fullstack Developer / Designer</h1>
+        <p>I humbly welcome you to my site! Here goes my story in Tech...</p>
+        <p>
+          {" "}
+          I started late in computing. Major reason being that even electricity
+          in my village was of rarity. The first time I owned a computer was in
+          my 14s. I got a windows xp computer and spend n number of hours
+          looking into it. I did not get to start early. I did my first program
+          in my college final year. This too, was due campus recruitment. But
+          there, I got my inspiration. My first sucessful program which was to
+          factorize a quadratic equation. What took me ages to calculate and
+          compile took seconds to program its logic. It not only simplified my
+          problems over the hour, It gave me a confidence that I too could
+          program. There I started my journey. After hours and hours of trials
+          and errors I learnt all creamy layers of tech, HTML, CSS and
+          JavaScript and then non creamy layers, Linux commands, deployments and
+          networks etc. The more I was in this, the more I wanted to move
+          forward. At times I appreciate that I get to start late cause by the
+          time I started, I had clear Idea what I should do and not do. This is
+          my story in tech.
+        </p>
+      </div>
     </div>
   );
 };
