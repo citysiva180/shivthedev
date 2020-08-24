332e234669cc642065af97df1d3531de146a2783
Sivarajan <citysiva180@gmail.com>
2020-08-24 23:56:22 +0530

    finally_completed_refresh_on_button

--- src/components/recentTweets.jsx
+++ src/components/recentTweets.jsx
@@ -1,48 +1,52 @@
 import React, { Component } from "react";
 import { Tweet } from "react-twitter-widgets";
 import "../stylesheets/recentTweets.css";
-// import {
-//   someTweet5,
-//   someTweet6,
-//   someTweet7,
-//   someTweet8,
-// } from "../components/rough";
+import { randomTweetGenerator } from "../components/rough";
 
 class Tweets extends Component {
   state = {
     tweets: [
-      "1124514496477687810",
-      "1128937546866544641",
-      "1134044618541027329",
-      "1140593481682833408",
-      "1183456431279788033",
-      "1266806025488748544",
+      { tweet: "1124514496477687810" },
+      { tweet: "1128937546866544641" },
+      { tweet: "1134044618541027329" },
+      { tweet: "1140593481682833408" },
     ],
   };
 
-  refreshPage = () => {
-    this.setState((state) => {});
+  reloadTweets = (e) => {
+    e.preventDefault();
+    let x = randomTweetGenerator();
+    let check = [...this.state.tweets];
+    for (let i = 0; i < check.length; i++) {
+      check[i].tweet = x[i];
+    }
+    this.setState({ check });
   };
+
   render() {
+    const renderTweet = (item, key) => {
+      return (
+        <div className="tweets">
+          {<Tweet tweetId={item.tweet} options={{ width: 200 }} />}
+        </div>
+      );
+    };
+
     return (
       <div className="Twitter">
         <div className="container">
           <h1>Tweets</h1>
-
+          <button className="btn btn-large purple" onClick={this.reloadTweets}>
+            Click here to randomly load some selected tweets
+          </button>
+          <p>{""}</p>
           <p>
             This component, randomly reloads selected tweets. For this you would
             need to get the twitter IDs and push them to a random function.{" "}
+            There is a lot of mechanism involved here. Preventing default load
+            and finally changing the state from normalcy
           </p>
-          {this.state.tweets.map((item) => (
-            <ul id="menu">
-              <li key={item}>
-                {<Tweet tweetId={item} options={{ width: 200 }} />}
-              </li>
-            </ul>
-          ))}
-          <button className="btn btn-large purple" onClick={this.refreshPage}>
-            Click here to randomly load some selected tweets
-          </button>
+          {this.state.tweets.map(renderTweet)}
         </div>
       </div>
     );
--- src/components/rough.js
+++ src/components/rough.js
@@ -1,33 +1,32 @@
-let tweetIds = [
-  "1281819161040023553",
-  "1289205851404660736",
-  "1108565412747186176",
-  "1118032374246322176",
-  "1118193735786008578",
-  "1119148595117035520",
-  "1124514496477687810",
-  "1128937546866544641",
-  "1134044618541027329",
-  "1140593481682833408",
-  "1183456431279788033",
-  "1266806025488748544",
-];
+export function randomTweetGenerator() {
+  const tweetIds = [
+    "1281819161040023553",
+    "1289205851404660736",
+    "1108565412747186176",
+    "1118032374246322176",
+    "1118193735786008578",
+    "1119148595117035520",
+    "1124514496477687810",
+    "1128937546866544641",
+    "1134044618541027329",
+    "1140593481682833408",
+    "1183456431279788033",
+    "1266806025488748544",
+  ];
 
-function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  let value1 = Number(randomIntFromInterval(0, 2));
+  let value2 = Number(randomIntFromInterval(3, 5));
+  let value3 = Number(randomIntFromInterval(6, 8));
+  let value4 = Number(randomIntFromInterval(9, 11));
+
+  let randomTweets = [];
+  randomTweets.push(tweetIds[value1]);
+  randomTweets.push(tweetIds[value2]);
+  randomTweets.push(tweetIds[value3]);
+  randomTweets.push(tweetIds[value4]);
+  return randomTweets;
 }
-
-let values = [];
-
-let value1 = Number(randomIntFromInterval(0, 2));
-let value2 = Number(randomIntFromInterval(3, 5));
-let value3 = Number(randomIntFromInterval(6, 8));
-let value4 = Number(randomIntFromInterval(9, 11));
-
-let someTweet5 = tweetIds[value1];
-let someTweet6 = tweetIds[value2];
-let someTweet7 = tweetIds[value3];
-let someTweet8 = tweetIds[value4];
-
-export { someTweet5, someTweet6, someTweet7, someTweet8 };
